ed6e193f54020a807649ad155c2272c721a116be
Arthur Volant <arthurvolant@gmail.com>
2021-03-31 12:53:46 +0200

    DEV: start working on cythonizing van der corput

--- scipy/stats/_qmc_cy.pyx
+++ scipy/stats/_qmc_cy.pyx
@@ -9,6 +9,8 @@ import numpy as np
 cimport numpy as np
 from libc.math cimport fabs, sqrt, pow
 
+from numpy.random cimport BitGenerator, SeedSequence
+
 np.import_array()
 
 cdef extern from "<thread>" namespace "std" nogil:
@@ -326,3 +328,32 @@ cdef void one_thread_loop(func_type loop_func, double& disc, double[:,
     threaded_sum_mutex.lock()
     (&disc)[0] += tmp # workaround to "disc += tmp", see cython issue #1863
     threaded_sum_mutex.unlock()
+
+
+cpdef double [:] van_der_corput_cy(Py_ssize_t n, np.uint32_t base=2,
+                                    Py_ssize_t start_index=0,
+                                    scramble=False, rng=None):
+    cdef:
+        double [:] sequence = np.zeros(n, dtype=np.double)
+        double [:] quotient = np.arange(start_index, start_index + n,
+                                        dtype=np.double)
+        double [:] remainder = np.zeros(n, dtype=np.double)
+        double b2r = 1 / <double> base
+
+    while (1 - b2r) < 1:
+        for i in range(n):
+            remainder[i] = quotient[i] % base
+        if scramble:
+            # permutation must be the same for all points of the sequence
+            perm = rng.permutation(base)
+            for i in range(n):
+                remainder[i] = perm[int(remainder[i])]
+
+        for i in range(n):
+            sequence[i] += remainder[i] * b2r
+
+        b2r /= base
+        for i in range(n):
+            quotient[i] = (quotient[i] - remainder[i]) / base
+
+    return sequence
